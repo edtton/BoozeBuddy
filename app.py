fb8838ba8cceab26ebf2d673784ac85728d535be
Ethan <ethandangton@gmail.com>
2024-03-24 04:10:06 -0400

    log out function

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from pymongo import MongoClient
-from propelauth_flask import init_auth
 from flask_login import LoginManager
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from flask import session
+from bson import ObjectId 
+import requests
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+from propelauth_flask import init_auth, current_user, current_org
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
@@ -24,8 +29,9 @@ def default():
         widmark = 0.68 
 
     bac = round(14 / (user.get('weight') * 453.6 * widmark) * 100, 2)
+    downtime = round((bac/0.015), 2)
 
-    return render_template('home-page-booze-buddy.html', name = name, gender = gender, weight = weight, height = height, user = user, widmark = widmark, bac = bac)
+    return render_template('home-page-booze-buddy.html', downtime = downtime, name = name, gender = gender, weight = weight, height = height, user = user, widmark = widmark, bac = bac)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -89,30 +95,88 @@ def dashboard():
 
     bac = round(14 / (user.get('weight') * 453.6 * widmark) * 100, 2)
 
-    return render_template('dashboard.html', user = user, widmark = widmark, bac = bac)
+    downtime = (bac/0.015)
+
+    return render_template('dashboard.html', user = user, widmark = widmark, bac = bac, downtime = downtime)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         field = request.form.get('field')
         query = request.form.get('query')
-        if field == 'name':
-            results = db.drinks.find({'name': {'$regex': query, '$options': 'i'}})
-        elif field == 'type':
-            results = db.drinks.find({'type': {'$regex': query, '$options': 'i'}})
-        elif field == 'abv':
-            results = db.drinks.find({'abv': {'$regex': query, '$options': 'i'}})
-        elif field == 'mixes':
-            results = db.drinks.find({'mixes': {'$regex': query, '$options': 'i'}})
+        if field:
+            if field == 'name':
+                results = db.drinks.find({'name': {'$regex': query, '$options': 'i'}})
+            elif field == 'type':
+                results = db.drinks.find({'type': {'$regex': query, '$options': 'i'}})
+            elif field == 'abv':
+                results = db.drinks.find({'abv': {'$regex': query, '$options': 'i'}})
+            elif field == 'mixes':
+                results = db.drinks.find({'mixes': {'$regex': query, '$options': 'i'}})
+            else:
+                results = None
         else:
-            results = None
-        return render_template('search.html', results = results)
-    return render_template('search.html')
+            # If no field is specified, return all entries
+            results = db.drinks.find()
+            query = "all drinks"
+        return render_template('search-results.html', results=results, query=query)
+    return render_template('search-results.html')
 
 @app.route('/resources')
 def resources():
     return render_template('resources-boozebuddy.html')
+
+@app.route('/details')
+def details():
+    drink_id = request.args.get('drink_id')
+
+    drink = db.drinks.find_one({'_id': ObjectId(drink_id)})
+
+    limit = round(0.6 / (drink.get('abv') / 100), 2)
+
+    return render_template('details.html', drink = drink, limit = limit)
+
+@app.route('/terms')
+def terms():
+    results = db.terms.find()
+    return render_template('terms.html', results = results)
+
+@app.route('/profile')
+def profile():
+    user = session.get('user')
+    return render_template('profile.html', user = user)
+
+@app.route('/like')
+def like():
+    user = session.get('user')
+    user_id = user.get('_id')
+    drink_id = request.args.get('drink_id')
+    drink = db.drinks.find_one({'_id': ObjectId(drink_id)})
+
+    db.users.update_one(
+        {"_id": ObjectId(user_id)},
+        {"$push": {"liked": drink_id}}
+    )
+
+    return render_template('search-results.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect(url_for('signup'))
+
+# @app.route('/update', methods=['POST'])
+# def update_user():
+#     # Retrieve form data
+#     age = request.form.get('age')
+#     gender = request.form.get('gender')
+#     height = request.form.get('height')
+#     weight = request.form.get('weight')
     
+
+
+#     return redirect(url_for('profile'))  
 
 if __name__ == '__main__':
     app.secret_key = 'hoohacks'
